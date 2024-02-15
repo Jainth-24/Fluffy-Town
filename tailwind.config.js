567fe21ffb3c50ae0ee2b92e82236d9347caec28
Jainth-24 <jainth.a@increscotech.com>
2024-02-15 16:21:04 +0530

    feat: next purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,11 @@ function withOpacity(variableName) {
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ "./node_modules/flowbite-react/lib/**/*.js",'./src/**/*.{js,ts,jsx,tsx}', './src/components/carousel/*.{tsx,ts,jsx,js}'],
+  content: [
+    './node_modules/flowbite-react/lib/**/*.js',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './src/components/carousel/*.{tsx,ts,jsx,js}',
+  ],
   theme: {
     extend: {
       colors: {
@@ -106,5 +110,28 @@ module.exports = {
       },
     },
   },
-  plugins: [require('flowbite/plugin')],
+  plugins: [
+    require('flowbite/plugin'),
+    'postcss-flexbugs-fixes',
+    [
+      'postcss-preset-env',
+      {
+        autoprefixer: {
+          flexbox: 'no-2009',
+        },
+        stage: 3,
+        features: {
+          'custom-properties': false,
+        },
+      },
+    ],
+    [
+      '@fullhuman/postcss-purgecss',
+      {
+        content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+        safelist: ['html', 'body'],
+      },
+    ],
+  ],
 };
